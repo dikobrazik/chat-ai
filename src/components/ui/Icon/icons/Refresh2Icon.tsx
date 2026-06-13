237d70e2da251450e4d704cae8f16d3d91b74884
ilnarik <ilnar.khusaenov@gmail.com>
2026-06-13 10:36:47 +0300

    svg fix

--- src/components/ui/Icon/icons/Refresh2Icon.tsx
+++ src/components/ui/Icon/icons/Refresh2Icon.tsx
@@ -6,9 +6,9 @@ export const RefreshIcon: React.FC<IconComponentProps> = (props) => (
     <path
       d="M14.6673 7.9987C14.6673 11.6787 11.6807 14.6654 8.00065 14.6654C4.32065 14.6654 2.07398 10.9587 2.07398 10.9587M2.07398 14.292V10.9587H5.08732M1.33398 7.9987C1.33398 4.3187 4.29398 1.33203 8.00065 1.33203C12.4473 1.33203 14.6673 5.0387 14.6673 5.0387M11.7073 5.0387H14.6673V1.70536"
       stroke="black"
-      stroke-width="1.1"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </BaseIcon>
 );
