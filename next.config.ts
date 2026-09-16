616806029b2330e77a43db081d75910be9597e77
ilnarik <ilnar.khusaenov@gmail.com>
2026-09-16 15:34:13 +0300

    домен для фоток из вк

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "*.vkuserphoto.ru",
+      },
+      {
+        protocol: "https",
         hostname: "nspk.ru",
       },
       {
